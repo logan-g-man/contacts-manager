fdc390a6a2bb8ff1835b16d2a74dd3ff4083c086
Kiyoshi Iwasaki <93811494+Kiyoshi1245@users.noreply.github.com>
2025-01-30 00:51:13 -0500

    DB connection utilizes .env variables versus hard-coded source code user/pass
    -Utilize commands stated in discord to allow communication between server and localhost
    
    -SQL server naturally blocks outside remotes without SSH tunneling due to firewall and security issues
    
    - Follow ENV variables stated in discord for success

--- LAMPAPI/DbConnection.php
+++ LAMPAPI/DbConnection.php
@@ -1,11 +1,27 @@
 <?php
-// LAMPAPI/DbConnect.php
+
+require __DIR__ . '/../vendor/autoload.php'; // Load composer dependencies
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load(); // Load .env file
+
 function getConnection()
 {
-	return new mysqli(
-		$_ENV['DB_HOST'],
-		$_ENV['DB_USER'],
-		$_ENV['DB_PASS'],
-		$_ENV['DB_NAME']
-	);
+    $mysqli = new mysqli
+    (
+        $_ENV['DB_HOST'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_NAME'],
+        $_ENV['DB_PORT']
+
+    );
+
+    if ($mysqli->connect_error) {
+        die("Database connection failed: " . $mysqli->connect_error);
+    }
+
+    return $mysqli; // Return the connection object
 }
+?>
